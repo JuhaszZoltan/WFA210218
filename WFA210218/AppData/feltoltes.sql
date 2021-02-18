84eac5a477676190002b91482fd94e6fb6f62e44
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2021-02-18 17:45:25 +0100

    Add project files.

---
+++ WFA210218/AppData/feltoltes.sql
@@ -0,0 +1,13 @@
+INSERT INTO gyak_tanulo VALUES
+('Kiss András', 11, 'A', 0),
+('Nagy Elek', 10, 'A', 0),
+('Közepes Ilona', 10, 'B', 1),
+('Szüre Tibor', 9, 'A', 0),
+('Alap Imre', 10, 'A', 0);
+
+INSERT INTO gyak_munka VALUES
+(1, 'V.ker Idősek Otthona', 'ágytálazás', '2017-11-02', 5),
+(1, 'Parkgondozó Közhaszontalan Társaság', 'virágültetés', '2017-11-12', 4),
+(2, 'Golyósportok Egyesület', 'Golyótisztítás', '2017-11-02', 5),
+(2, 'V.ker Idősek Otthona', 'ágytáltisztítás', '2017-12-02', 5),
+(3, 'Szabó Ervin Könyvtár', 'Létracipelés', '2017-12-02', 3);
